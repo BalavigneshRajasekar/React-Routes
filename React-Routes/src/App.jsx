3428a8c9e720547afd4f0cf37e7e9e024c9c87c9
BalavigneshRajasekar <you@example.com>
2024-05-14 23:28:09 +0530

    Title changed

--- React-Routes/src/App.jsx
+++ React-Routes/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Routers from "./Routers";
-import FullStackDevelopment from "./FullStackDevelopment";
 
 function App() {
   return (
